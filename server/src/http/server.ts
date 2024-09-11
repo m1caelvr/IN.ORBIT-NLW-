2f3ff8180524e873d49c25796f1a67ad749277e7
Micael-Rodrigues <micaelvitor709@gmail.com>
2024-09-11 18:09:21 -0300

    Creating server http initialization

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,1 +1,11 @@
-console.log("Funcionou!")
+import fastify from 'fastify'
+
+const app = fastify()
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('Server is running!')
+  })
